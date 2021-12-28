b451d83e63c3dec46c1d3988132671d512c22184
vishalverma1999 <vermavishal1999.vv@gmail.com>
2021-12-28 14:37:41 +0530

    User information components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,7 @@
 import Chart from '../../components/chart/Chart'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
+import WidgetLarge from '../../components/widgetLarge/WidgetLarge'
+import WidgetSmall from '../../components/widgetSmall/WidgetSmall'
 import { Userdata } from '../../data'
 import './home.css'
 
@@ -8,6 +10,10 @@ function Home() {
         <div className='home'>
             <FeaturedInfo/>
             <Chart title="User Analytics" data={Userdata} dataKey="Active Users" grid/>
+            <div className="homeWidgets">
+                <WidgetSmall/>
+                <WidgetLarge/>
+            </div>
         </div>
     )
 }
